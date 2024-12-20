4fe53f477d263243e316c16a1e96be9757764a3a
baehl <baehl44@yahoo.fr>
2024-12-20 12:21:16 +0100

    creation fonction startGame2 pour gerer partie du jeu avec 4 couleurs à deviner avec une palette de 8 couleurs

--- mastermind.js
+++ mastermind.js
@@ -69,6 +69,24 @@ function codeBreakerFour(color1, color2,color3,color4) {
     }
     return false
 }
-console.log(codeBreakerFour('red','black','white','grey'))//false
-console.log(codeBreakerFour('red','black','purple','yellow'))// false
-console.log(codeBreakerFour('red','pink','green','black'))//true
+// console.log(codeBreakerFour('red','black','white','grey'))//false
+// console.log(codeBreakerFour('red','black','purple','yellow'))// false
+// console.log(codeBreakerFour('red','pink','green','black'))//true
+
+function startGame2(color1, color2,color3,color4) {
+    if (nbOfTry < maxTry) {
+      if (handle8Colors(color1, color2,color3,color4) === false) {
+        return "veuillez choisir une des 8 couleurs proposées";
+      } else if (codeBreakerFour(color1, color2,color3,color4) === false) {
+        nbOfTry++;
+        return `veuillez réessayer, tentative ${nbOfTry}`;
+      }
+      return "Bravo vous avez réussi à devener les 4 couleurs";
+    }
+    return "Vous avez atteint le max des tentatives, les coleurs à deviner sont (red,pink,green,black)";
+  }
+  //console.log(startGame2('red','pink','green'))
+  //console.log(startGame2('red','pink','green','black'))
+  //console.log(startGame2('red','yellow','green','black')) 
+
+
