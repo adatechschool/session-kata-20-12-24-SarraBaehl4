ed1a977a99070c4c0a67ab71f5eb541f0f095412
baehl <baehl44@yahoo.fr>
2024-12-20 10:34:25 +0100

    creation et validation de la 1ere fonction handleInput pour la gestion de la saisie des 4 couleurs

--- mastermind.js
+++ mastermind.js
@@ -1,1 +1,22 @@
-alert ('hello')
+//** Étape 1:
+
+let colorsToGuess = ["red", "black"];
+const maxTry = 12;
+let nbOfTry = 0;
+const acceptedColors = ["red", "pink", "green", "black"];
+
+//fonction pour s'assurer que les 4 couleurs sont respectées ou non:
+function handleInput(color1, color2) {
+    let color1IsValide = false
+    let color2IsValide = false
+  for (let i = 0; i < acceptedColors.length; i++) {
+    if (color1 === acceptedColors[i]) {
+      color1IsValide = true;
+    }
+    if (color2 === acceptedColors[i]) {
+      color2IsValide = true;
+    }
+  }
+  return color1IsValide && color2IsValide;
+}
+console.log(handleInput("red", "pink"));
