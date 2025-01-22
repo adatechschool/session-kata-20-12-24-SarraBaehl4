a03657071585bf0d36f0f06c50de2442c2d10cad
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2025-01-22 15:42:59 +0100

    Remarque de code review

--- mastermind.js
+++ mastermind.js
@@ -7,10 +7,10 @@ const acceptedColors = ["red", "pink", "green", "black"];
 
 //fonction pour s'assurer que les 4 couleurs sont respectées ou non:
 function handleacceptedColors(color1, color2) {
-  if (color1 == undefined && color2 == undefined){
+  if (color1 == undefined && color2 == undefined){ // JFG -> Peut être réécrit de cette manière if (!color1 && !color2) mais ce que tu veux tester c'est un OU logique et pas un ET
     return false
   }
-  if (typeof color1 !== 'string' && typeof color2 !== 'string'){
+  if (typeof color1 !== 'string' && typeof color2 !== 'string'){ // JFG -> Ici aussi, tu veux en vrai tester un OU Logique et pas un ET
     return false
   }
   return acceptedColors.includes(color1) && acceptedColors.includes(color2);
@@ -22,7 +22,7 @@ function handleacceptedColors(color1, color2) {
 
 //Une fonction qui retourne true ou false si la bonne combinaison est trouvée ou non:
 function codeBreaker(color1, color2) {
-    if (color1 === colorsToGuess[0] && color2 === colorsToGuess[1]){
+    if (color1 === colorsToGuess[0] && color2 === colorsToGuess[1]){ // JFG -> Tu peux ici retourner direct return color1 === colorsToGuess[0] && ...
         return true
     }
     return false
@@ -34,7 +34,7 @@ function codeBreaker(color1, color2) {
 
 
 //Une fonction qui gère la partie (continuer tant que/fin si gagné)
-function startGame(color1, color2) {
+function startGame(color1, color2) { // JFG -> Cette méthode n'exploite pas de tant que
   if (nbOfTry < maxTry) {
     if (handleacceptedColors(color1, color2) === false) {
       return "veuillez choisir une des 4 couleurs proposées";
@@ -72,7 +72,7 @@ function handle8Colors(color1, color2, color3, color4) {
 //console.log(handle8Colors('red','black','white','grey'))//false
 //console.log(handle8Colors('red','black','purple','yellow'))// true
 function codeBreakerFour(color1, color2,color3,color4) {
-    if (color1 === fourColorsToGuess[0] && color2 === fourColorsToGuess[1] && color3 === fourColorsToGuess[2] && color4 === fourColorsToGuess[3]){
+    if (color1 === fourColorsToGuess[0] && color2 === fourColorsToGuess[1] && color3 === fourColorsToGuess[2] && color4 === fourColorsToGuess[3]){ // JFG -> Même remarque sur le return direct
         return true
     }
     return false
@@ -81,7 +81,7 @@ function codeBreakerFour(color1, color2,color3,color4) {
 // console.log(codeBreakerFour('red','black','purple','yellow'))// false
 // console.log(codeBreakerFour('red','pink','green','black'))//true
 
-function startGame2(color1, color2,color3,color4) {
+function startGame2(color1, color2,color3,color4) { // JFG -> Même remarque sur la non présence du tant que
     if (nbOfTry < maxTry) {
       if (handle8Colors(color1, color2,color3,color4) === false) {
         return "veuillez choisir une des 8 couleurs proposées";
